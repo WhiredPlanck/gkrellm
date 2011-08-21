f0f4c074a8c5eeab7bbb67e3f891e9ecd5819fc3
Stefan Gehn <stefan@srcbox.net>
2011-08-21 18:02:57 +0200

    Add function to gracefully disconnect clients
    Add gkrellmd_client_close() function that closes the socket connection
    and runs a callback on success. The callback allows to easily free the
    GKrellmdClient once it has properly disconnected.

--- server/gkrellmd.h
+++ server/gkrellmd.h
@@ -139,6 +139,8 @@ GKRELLMD_VERSION_REV >= (rev)))
 
 typedef struct _GkrellmdClient GkrellmdClient;
 
+typedef void (*GkrellmdClientFunc)(GkrellmdClient *client, gpointer user_data);
+
 typedef void (*GkrellmdClientReadFunc)(GkrellmdClient *client, GString *str,
 		gpointer user_data);
 
@@ -163,6 +165,8 @@ typedef struct _GkrellmdClient
 	gpointer read_func_user_data;
 	GString	*write_buf;
 	GSource	*write_source;
+	GkrellmdClientFunc close_func;
+	gpointer close_func_user_data;
 	}
 	GkrellmdClient;
 
